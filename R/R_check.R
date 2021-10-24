37dba77e8357dcc26bd4b8b65820031d202f4120
Hwankam <fbghksrka10@naver.com>
2021-10-24 15:55:48 +0900

    R

--- R/R_check.R
+++ R/R_check.R
@@ -21,3 +21,17 @@ kk
 #5 list, unlist
 ## List has nested structure. So to use the argument conveniently we can use unlist
 
+#6 histogram and density plot together
+hist(x, probability=T)
+
+y <- seq(from=5,to=8,by=0.01)
+fy <- dnorm(y, mean=7, sd=0.1)
+lines(y,fy)
+## we put the argument "probability=T" to alter y-axis to probability
+
+#7 When I want to find variable corresponding with some condition
+Table$variable1[variable2 == condition]
+
+#8 plot together
+par(new=T)
+plot(x=x , y=y, ann=F, axes=F )
